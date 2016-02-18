1b0da1b23ec03403d554dd4ec67c235144e57a56
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2016-02-18 16:20:20 +0100

    Added price boxes

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,48 +1,6 @@
-'use strict';
-angular.module('invoiceApp', [
-    'ui.router'
-])
+(function(window, document, undefined) {
+	'use strict';
 
-    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise('/home');
+	console.log('Loaded!');
 
-        $stateProvider
-
-        .state('home', {
-            url: '/home',
-            //controller: 'postsList',
-            templateUrl: 'views/home/home-view.html',
-            resolve: {
-				auth: function(UserFactory) {
-					var auth = UserFactory.auth();
-					console.log('auth: ', auth);
-					return auth;
-				}
-            }
-        })
-		// status
-        .state('invoices', {
-            url: '/invoices',
-            controller: 'invoiceController',
-            templateUrl: 'views/invoices/invoice.html',
-            resolve: {
-                status: function(UserFactory) {
-					return UserFactory.status();
-				},
-				invoices: function(InvoiceFactory) {
-					return InvoiceFactory.getInvoices();
-				}
-            }
-        });
-        //.state('posts.detail', {
-        //    url: '/detail/:id',
-        //    controller: 'postsList',
-        //    templateUrl: 'views/posts/postsList.html',
-        //    resolve: {
-        //        post: function(postsFactory, $stateParams) {
-        //            console.log('Post:', postsFactory.getById({id: $stateParams.id}));
-        //            return postsFactory.getById({id: $stateParams.id});
-        //        }
-        //    }
-        //})
-    }]);
+})(window, document);
